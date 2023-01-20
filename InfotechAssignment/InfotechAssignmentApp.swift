75062e6c134d05e2b15f5192daba2c7245bcdc66
ErikK <erik.giertl@icloud.com>
2023-01-20 15:16:48 +0100

    Initial Commit

---
+++ InfotechAssignment/InfotechAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfotechAssignmentApp.swift
+//  InfotechAssignment
+//
+//  Created by Erik Kokinda on 20/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct InfotechAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
